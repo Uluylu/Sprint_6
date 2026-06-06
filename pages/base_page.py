d65ff4acf3143285046c11e666c5eb559c04bf61
Valeriy <vplotarev@inbox.ru>
2026-06-06 11:26:31 +0400

    fix comments

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
@@ -6,22 +7,28 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
+    @allure.step("Ждать появления {locator} на странице")
     def find_element(self, locator):
         return WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located(locator))
 
+    @allure.step("Выполнить клик по элементу с локатором: {locator}")
     def click_element(self, locator):
         self.find_element(locator).click()
 
+    @allure.step("Заполнить поле {locator} значением: {text}")
     def fill_field(self, locator, text):
         element = self.find_element(locator)
         element.send_keys(text)
 
+    @allure.step("Прокрутить страницу до элемента с локатором: {locator}")
     def scroll_to_element(self, locator):
         element = self.find_element(locator)
         self.driver.execute_script("arguments[0].scrollIntoView(false);", element)
 
+    @allure.step("Открыть страницу по адресу: {url}")
     def open_url(self, url):
         self.driver.get(url)
 
+    @allure.step("Получить текущий URL браузера")
     def get_current_url(self):
         return self.driver.current_url
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,9 @@
 import allure
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 from pages.base_page import BasePage
+from urls import Urls
 
 
 class MainPage(BasePage):
@@ -15,19 +18,27 @@ class MainPage(BasePage):
     def accept_cookies(self):
         self.click_element(self.COOKIE_ACCEPT_BUTTON)
 
-    @allure.step("Кликнуть по верхней кнопке «Заказать»")
+    @allure.step("Кликнуть по верхней кнопке 'Заказать'")
     def click_top_button_order(self):
         self.click_element(self.TOP_BUTTON_ORDER)
 
-    @allure.step("Прокрутить страницу и кликнуть по нижней кнопке «Заказать»")
+    @allure.step("Прокрутить страницу и кликнуть по нижней кнопке 'Заказать'")
     def click_bottom_button_order(self):
         self.scroll_to_element(self.BOTTOM_BUTTON_ORDER)
         self.click_element(self.BOTTOM_BUTTON_ORDER)
 
-    @allure.step("Кликнуть по логотипу «Самокат»")
+    @allure.step("Кликнуть по логотипу 'Самокат'")
     def click_scooter_logo(self):
         self.click_element(self.SCOOTER_LOGO)
 
-    @allure.step("Кликнуть по логотипу «Яндекс»")
+    @allure.step("Кликнуть по логотипу 'Яндекс'")
     def click_yandex_logo(self):
         self.click_element(self.YANDEX_LOGO)
+
+    @allure.step("Переключиться на новую вкладку")
+    def switch_to_new_tab(self):
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+
+    @allure.step("Дождаться загрузки страницы Дзена")
+    def wait_for_dzen_url(self):
+        WebDriverWait(self.driver, 5).until(expected_conditions.url_contains(Urls.DZEN_URL))
